d64220d1792fdcec2c31bd7309fae62d2099f9ac
Miguel A. M <engmiguelmonteiro@gmail.com>
2022-08-29 19:56:31 -0300

    Commit inicial

---
+++ project_locus/src/main/java/com/projectlocus/webservice/ProjectLocusApplication.java
@@ -0,0 +1,13 @@
+package com.projectlocus.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectLocusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectLocusApplication.class, args);
+	}
+
+}
